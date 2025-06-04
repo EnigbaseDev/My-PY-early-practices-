25036ad0035cef4a8aa7191c174543b49392dcf1
EnigbaseDev <209428637+EnigbaseDev@users.noreply.github.com>
2025-06-04 14:31:27 -0500

    Numeros primos Exercise

---
+++ practices/numerosprimos.py
@@ -0,0 +1,19 @@
+
+def is_primo():
+
+    for number in range(1, 101):
+
+        if number >= 2:
+
+            is_divisible = False
+
+            for index in range(2, number):
+                if number % index == 0:
+                    is_divisible = True
+                    break
+
+            if not is_divisible:
+                print(number)
+
+
+is_primo()
